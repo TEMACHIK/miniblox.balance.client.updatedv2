6615d875b27393ad2c7b94ec7185433d6bdbd951
TEMACHIK <sergienkoartem159@gmail.com>
2026-01-13 17:12:19 +0200

    Update tampermonkey.user.js
    Signed-off-by: TEMACHIK <sergienkoartem159@gmail.com>

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,21 +1,21 @@
 // ==UserScript==
-// @name         Impact For Miniblox
-// @namespace    https://github.com/progmem-cc
-// @version      8
-// @description  The ultimate Miniblox hacked client which is built for total domination of all Miniblox servers with a fully dark-mode optimized modern GUI and high performance guaranteed.
-// @author       7GrandDadPGN, ProgMEM-CC, TheM1ddleM1n, 6x68 (bab), dtkiller-jp
+// @name         Balance Dlc For Miniblox
+// @namespace    https://github.com/TEMACHIK
+// @version      9
+// @description  The ultimate Miniblox hacked client which is built for total domination of all Miniblox servers.
+// @author       Temachik
 // @match        https://miniblox.io/*
 // @match        https://miniblox.org/*
 // @match        https://miniblox.online/*
 // @match        https://bloxbattles.io/*
-// @icon         https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/refs/heads/main/favicon.png
+// @icon         https://raw.githubusercontent.com/TEMACHIK/miniblox.impact.client.updatedv2/refs/heads/main/favicon.png
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        unsafeWindow
-// @require      https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/refs/heads/main/vav4inject.js
-// @require      https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/refs/heads/main/unlocker.js
+// @require      https://raw.githubusercontent.com/TEMACHIK/miniblox.impact.client.updatedv2/refs/heads/main/vav4inject.js
+// @require      https://raw.githubusercontent.com/TEMACHIK/miniblox.impact.client.updatedv2/refs/heads/main/unlocker.js
 // @run-at       document-start
-// @updateURL    https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
-// @downloadURL  https://raw.githubusercontent.com/progmem-cc/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
+// @updateURL    https://raw.githubusercontent.com/TEMACHIK/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
+// @downloadURL  https://raw.githubusercontent.com/TEMACHIK/miniblox.impact.client.updatedv2/main/tampermonkey.user.js
 // @license      AGPL-3.0-only
 // ==/UserScript==
